570b1c389d2138610cd1338da7dbe3bc2047afe7
BT_3W2_11 <raymondg89@nycstudents.net>
2022-10-19 09:18:50 -0400

    Initial commit

---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a coordinate point");
+        String firstPoint = input.nextLine();
+        System.out.println("Please enter another coordinate point");
+        String secondPoint = input.nextLine();
+
+        int stringAsInt = Integer.parseInt("57");
+        System.out.println(stringAsInt);
+    }
+}
