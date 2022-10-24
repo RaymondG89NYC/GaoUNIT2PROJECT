2be50514b41afd574120f1bdd127d39ec36f62a8
BT_3W2_11 <raymondg89@nycstudents.net>
2022-10-24 09:11:39 -0400

    10/24

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -11,6 +11,34 @@ public class LinearEquation {
         this.y2 = y2;
     }
 
+    public double roundNumber(double number){
+        number *= 100;
+        number = Math.round(number);
+        return number/100;
+    }
+
+    public double distance(){
+        double distance = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+        return(roundNumber(distance));
+    }
+
+    public double yIntercept(){
+        double y = y1;
+        double x = x1;
+
+        y -= x*slope();
+        return(roundNumber(y));
+    }
+
+    public double slope(){
+        double run = x2-x1;
+        double rise = y2 - y1;
+        double slope = rise/run;
+        return(roundNumber(slope));
+    }
+
+
+
 
 
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -10,14 +10,27 @@ public class LinearEquationRunner {
         //parseInt'ing the coordinate points
         int commaLocation1 = firstPoint.indexOf(",");
         int x1 = Integer.parseInt(firstPoint.substring(1,commaLocation1));
-        int y1 = Integer.parseInt(firstPoint.substring(commaLocation1+1, firstPoint.length()-1));
+        int y1 = Integer.parseInt(firstPoint.substring(commaLocation1+2, firstPoint.length()-1));
 
         int commaLocation2 = secondPoint.indexOf(",");
         int x2 = Integer.parseInt(secondPoint.substring(1,commaLocation2));
-        int y2 = Integer.parseInt(secondPoint.substring(commaLocation2+1, secondPoint.length()-1));
+        int y2 = Integer.parseInt(secondPoint.substring(commaLocation2+2, secondPoint.length()-1));
+
+        System.out.println(x1);
+        System.out.println(y1);
+        System.out.println(x2);
+        System.out.println(y2);
+        System.out.println();
+        System.out.println(y2 - y1);
+        System.out.println(x2 - x1);
+        System.out.println();
+
         //creating the class
         LinearEquation linearEquationRunner = new LinearEquation(x1, y1, x2, y2);
 
+//        System.out.println(linearEquationRunner.distance());
+//        System.out.println(linearEquationRunner.slope());
+        System.out.println(linearEquationRunner.yIntercept());
 
     }
 }
